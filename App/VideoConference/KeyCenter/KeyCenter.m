6ed95ed40921fea12a37daa65e2216a56eca731a
ZYP <zhuyuping@agora.io>
2021-08-07 23:23:57 +0800

    update keyCenter

---
+++ App/VideoConference/KeyCenter/KeyCenter.m
@@ -0,0 +1,13 @@
+#import "KeyCenter.h"
+
+@implementation KeyCenter
+
++ (NSString *)agoraAppid {
+    return <#agoraAppid#>;
+}
+
++ (NSString *)appCertificate {
+    return <#appCertificate#>;
+}
+
+@end
